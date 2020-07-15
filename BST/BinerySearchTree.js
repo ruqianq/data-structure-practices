c154b3b63b64665773d200248e831ac5fb739897
Daisy Lin <daisy@acts.net>
2020-07-15 10:18:13 -0400

    Create BinerySearchTree.js

---
+++ BST/BinerySearchTree.js
@@ -0,0 +1,126 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinerySearchTree {
+    constructor() {
+        this.root = null
+    }
+    insert(val) {
+        if (!this.root) {
+            this.root = new Node(val)
+            return this
+        }
+        let cur = this.root
+        while (cur.right && cur.left) {
+        if (val > cur.value) {
+            cur = cur.right
+        } else {
+            cur = cur.left
+        }
+        }
+        if (val > cur.value) {
+            cur.right = new Node(val)
+        } else {
+            cur.left = new Node(val)
+        }
+        return this
+    }
+    find(val) {
+        if (!this.root) {
+            return false
+        }
+        let cur = this.root;
+        let found = false
+        while (cur && !found) {
+            if (val > cur.value) {
+                cur = cur.right
+            } else if (val < cur.value) {
+                cur = cur.left
+            } else {
+                found = true
+            }
+        }
+        if (!found) {
+            return false
+            }
+        return cur
+    }
+    bfs() {
+        let data = [];
+        let queue = [];
+        let node = this.root
+        queue.push(node)
+        while(queue.length){
+            node = queue.shift()
+            data.push(node.value)
+            if (node.left) {
+                queue.push(node.left)
+            }
+            if (node.right) {
+                queue.push(node.right)
+            }
+
+        }
+        return data
+    }
+    dfs_pre(){
+        let data = [];
+        let cur = this.root;
+        function helper(node) {
+            data.push(node.value)
+            if (node.left) {
+                helper(node.left)
+            }
+            if (node.right) {
+                helper(node.right)
+            }
+        }
+        helper(cur)
+        return data
+    }
+    dfs_post(){
+        let data = [];
+        let cur = this.root;
+        function helper(node) {
+            if (node.left) {
+                helper(node.left)
+            }
+            if (node.right) {
+                helper(node.right)
+            }
+            data.push(node.value)
+        }
+        helper(cur)
+        return data
+    }
+    dfs_order() {
+        let data = [];
+        let cur = this.root;
+        function helper(node) {
+            if (node.left) {
+                helper(node.left)
+            }
+            data.push(node.value)
+            if (node.right) {
+                helper(node.right)
+            }
+        }
+        helper(cur)
+        return data
+    }
+}
+
+let test = new BinerySearchTree()
+test.insert(10)
+test.insert(15)
+test.insert(6)
+test.insert(20)
+test.insert(3)
+test.insert(13)
+test.insert(7)
+test.dfs_order()
