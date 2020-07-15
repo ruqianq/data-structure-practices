8a8e44f328bcd8b0217f6dc8c04bbf1e94aba05d
Daisy Qin <dqin@onyxgs.com>
2020-07-15 11:23:01 -0400

    add the MaxBinaryHeap.js

---
+++ BinaryHeap/MaxBinaryHeap.js
@@ -0,0 +1,23 @@
+function insert(arr, num) {
+  arr.push(num)
+  return bubbleUp(arr, arr.length - 1)
+}
+
+function bubbleUp(arr, childIndex) {
+  let parentIndex = Math.floor((childIndex - 1)/2)
+  if (parentIndex < 0) {
+    return arr
+  }
+  if (arr[childIndex] > arr[parentIndex]) {
+    let swapedArr = swap(arr, parentIndex, childIndex)
+    childIndex = parentIndex
+    bubbleUp(swapedArr, childIndex)
+  }
+  return arr
+}
+
+function swap(arr, parentIndex, childIndex) {
+  return [arr[parentIndex], arr[childIndex]] = [arr[childIndex], arr[parentIndex]]
+}
+
+insert([6, 4, 2], 1)
