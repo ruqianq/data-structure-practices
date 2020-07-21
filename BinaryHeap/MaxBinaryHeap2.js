84f62c4d2b1f11124bab2de1c8c1e8e8bb7f2eaa
Daisy Qin <dqin@onyxgs.com>
2020-07-21 09:07:50 -0400

    add the MaxBinaryHeap2.js

---
+++ BinaryHeap/MaxBinaryHeap2.js
@@ -0,0 +1,23 @@
+class MaxBinaryHeap2 {
+  constructor() {
+    this.values = []
+  }
+
+  insert(ele) {
+    this.values.push(ele)
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1)/2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+}
