3a80c2ff2097ad7f8313cf6a10a3ce7afa59e966
Daisy Qin <dqin@onyxgs.com>
2020-07-27 09:43:02 -0400

    add extractMax to MaxBinaryHeap.js

--- BinaryHeap/MaxBinaryHeap.js
+++ BinaryHeap/MaxBinaryHeap.js
@@ -21,4 +21,30 @@ function swap(arr, parentIndex, childIndex) {
   return arr
 }
 
+function extractMax(arr) {
+  let swappedArr = swap(arr, 0, arr.length - 1)
+  swappedArr.pop()
+  adjust(swappedArr, 0)
+  return swappedArr
+}
+
+function adjust(arr, idx) {
+
+  let leftIdx = idx * 2 + 1
+  let rightIdx = idx * 2 + 2
+
+  while(idx < arr.length - 1) {
+    if (arr[rightIdx] > arr[leftIdx]) {
+      if (arr[idx] > arr[rightIdx]) break;
+      arr = swap(arr, rightIdx, idx)
+      idx = rightIdx
+    } else {
+      if (arr[idx] > arr[leftIdx]) break;
+      arr = swap(arr, leftIdx, idx)
+      idx = leftIdx
+    }
+  }
+  return arr
+}
+
 insert([6, 4, 2], 1)
