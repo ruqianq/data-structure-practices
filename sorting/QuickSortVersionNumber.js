54c67031f361ff91ff899585ba1b4a393a8bab46
Daisy Qin <dqin@onyxgs.com>
2020-08-21 11:34:54 -0400

    Add new file of sorting

---
+++ sorting/QuickSortVersionNumber.js
@@ -0,0 +1,13 @@
+const versionList = ['12.0.1', '12.12.1', '13.0.1', '10.0.1', '9.9.10', '8.12.10'];
+
+const testArr1 = ['9.9.10', '8.12.10']
+const testArr2 = ['13.0.1', '10.0.1']
+
+function transformVersionNumber(versionNum) {
+  let versionNumArr = versionNum.split('.')
+  for (let i of versionNumArr) {
+    if (i.length < 2) {
+
+    }
+  }
+}
