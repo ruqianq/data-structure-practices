19a2e2981673c51d66fe157e411c9e73d248d7c5
Daisy Qin <dqin@onyxgs.com>
2020-08-22 07:43:48 -0400

    Add new file of sorting

--- sorting/SortVersionNumber.js
+++ sorting/SortVersionNumber.js
@@ -1,16 +1,18 @@
 const versionList = ['12.0.1', '12.12.1', '13.0.1', '10.0.1', '9.9.10', '8.12.10'];
 
-const testArr1 = ['9.9.10']
-
-function transformVersionNumber(versionNum) {
+function normalize(versionNum) {
   let versionNumArr = versionNum.split('.')
   let result = []
   for (let i of versionNumArr) {
     if (i.length < 2) {
       i = '0' + i
-
     }
     result.push(i)
   }
-  return result.join()
+
+  return parseInt(result.join("")) + 1000000
+}
+
+function denormalize(versionNum) {
+
 }
