bce543c2c5d57ef8d5fec0fd1dcb6eb99e06edb8
Daisy Qin <dqin@onyxgs.com>
2020-09-02 16:47:14 -0400

    rename follow convention

--- Graph/FindIsland.js
+++ Graph/FindIsland.js
@@ -14,18 +14,29 @@ class Island {
     this.graph = graph;
   }
 
-  dfs(r, c, visited) {
-    if (r < 0 || c < 0 || r >= this.row || c >= this.col || this.graph[r][c] != 1) {
-      return null
+  dfs(r, c) {
+    if (r < 0 || c < 0 || r >= this.row || c >= this.col || this.graph[r][c] !== 1) {
+      return
     }
 
-    this.graph[c][r] = "#"
+    this.graph[r][c] = "#"
     this.dfs(r + 1, c, this.graph)
     this.dfs(r - 1, c, this.graph)
     this.dfs(r, c + 1, this.graph)
     this.dfs(r, c - 1, this.graph)
   }
-
+  count() {
+    let result = 0;
+    for (let i = 0; i < this.row; i++) {
+      for (let j = 0; j < this.row; j++) {
+        if (this.graph[i][j] === 1) {
+          this.dfs(i, j)
+          result += 1
+        }
+      }
+    }
+    return result
+  }
 }
 
 const island = new Island(met.length, met[0].length, met)
