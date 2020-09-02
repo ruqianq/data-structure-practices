5a241a127e098ad872e4e37d9d8798975d7229c7
Daisy Qin <dqin@onyxgs.com>
2020-09-02 13:45:02 -0400

    add the sample graph

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -33,4 +33,23 @@ class Graph {
     }
     delete this.adjacencyList[vertex]
   }
+  DFS(vertex) {
+
+  }
 }
+
+g = new Graph()
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
