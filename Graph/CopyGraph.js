0bbbdc7a69353295d2595192f58ef7931177bd63
Daisy Qin <dqin@onyxgs.com>
2020-09-11 08:40:31 -0400

    add the copy function

--- Graph/CopyGraph.js
+++ Graph/CopyGraph.js
@@ -1,5 +1,26 @@
 // Given the root node of a directed graph, clone this graph by creating its deep copy so that the cloned graph has
 // the same vertices and edges as the original graph.
 
+class Graph {
+  constructor() {
+    this.adjacencyList = {}
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = []
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2)
+    this.adjacencyList[vertex2].push(vertex2)
+  }
+
+  copy(root) {
+    let newGraph = new Graph()
+  }
+}
+
 
 
