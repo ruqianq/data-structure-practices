674d7968ba68fdad63e3fbcaf40797a14723689c
Daisy Qin <dqin@onyxgs.com>
2020-09-11 10:51:18 -0400

    revise clone function

--- Graph/CopyGraph.js
+++ Graph/CopyGraph.js
@@ -1,4 +1,4 @@
-// Given the root node of a directed graph, clone this graph by creating its deep copy so that the cloned graph has
+// Given the root node of a undirected graph, clone this graph by creating its deep copy so that the cloned graph has
 // the same vertices and edges as the original graph.
 
 function Node(val, neighbors) {
