8ae59b293d1da9615adef3b559306757d8876180
ruqianq <daisy@acts.net>
2021-06-15 20:19:16 -0400

    add the new question

---
+++ BST/DeepestLeavesSum.js
@@ -0,0 +1,18 @@
+// Given the root of a binary tree,
+// return the sum of values of its deepest leaves.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+export function deepestLeavesSum(root) {
+
+}
--- tests/CountPointInsideCircle.test.ts
+++ tests/CountPointInsideCircle.test.ts
@@ -1,22 +1,27 @@
-import {pointToCenter, countPoints, isPointInside, isPointInsideCircleCounter} from "../Pointer/CountPointsInsideCircle";
+import {
+    pointToCenter,
+    countPoints,
+    isPointInside,
+    isPointInsideCircleCounter
+} from "../Pointer/CountPointsInsideCircle";
 
 test('Test point to center of circle distance', () => {
     expect(pointToCenter([1, 1], [1, 2, 2])).toEqual(1);
 });
 
 test('Test the point out of circle should return False', () => {
-    expect(isPointInside([3,3], [1,2,2])).toBe(false);
+    expect(isPointInside([3, 3], [1, 2, 2])).toBe(false);
 });
 
 test('Test the point at the boundary of circle should return True', () => {
-    expect(isPointInside([1,3], [2,3,1])).toBe(true);
+    expect(isPointInside([1, 3], [2, 3, 1])).toBe(true);
 });
 
 test('Test if the point inside the circle then count 1', () => {
-    expect(isPointInsideCircleCounter([1,3], [2,3,1], 0)).toBe(1);
+    expect(isPointInsideCircleCounter([1, 3], [2, 3, 1], 0)).toBe(1);
 });
 
 test('Test the multiple points weather it is inside the circles', () => {
-    expect(countPoints([[1,3],[3,3],[5,3],[2,2]],
-        [[2,3,1],[4,3,1],[1,1,2]])).toEqual([3,2,2]);
+    expect(countPoints([[1, 3], [3, 3], [5, 3], [2, 2]],
+        [[2, 3, 1], [4, 3, 1], [1, 1, 2]])).toEqual([3, 2, 2]);
 });
