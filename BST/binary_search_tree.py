7a49016cb45d78b732efcd70f4e1d40c0b2b0b04
ruqianq <daisy@acts.net>
2021-07-06 14:56:38 -0400

    add the binary_search_tree.py

--- BST/binary_search_tree.py
+++ BST/binary_search_tree.py
@@ -7,3 +7,16 @@ class BinarySearchTree:
     def insert(self, value):
         if not self.root:
             self.root = Node(value=value)
+        cur = self.root
+        while cur.left and cur.right:
+            if value > cur.value:
+                cur = cur.right
+            else:
+                cur = cur.left
+
+        if value > cur.value:
+            cur.right = Node(value)
+        else:
+            cur.left = Node(value)
+
+        return self
